9c6cece1b4392f86bafaf892607a568d2c6b72f7
Leonardo Brito <leobri93@gmail.com>
2015-04-27 10:05:41 -0300

    First Commit

---
+++ src/buscaia/BuscaIA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaia;
+
+
+public class BuscaIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    //Primeiro trabalho de IA: Desenvolver três métodos de busca entre dois pontos//
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
