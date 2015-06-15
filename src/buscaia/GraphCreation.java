ad65347bedc709c7b810c33e6a18950adaf32cc5
Leonardo Brito <leobri93@gmail.com>
2015-06-14 21:56:20 -0300

    Evolução

--- src/buscaia/GraphCreation.java
+++ src/buscaia/GraphCreation.java
@@ -12,14 +12,36 @@ import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
 
 public class GraphCreation {
+    /**
+     * Retorna um Grafo preenchido com todos os nós e suas arestas
+     * @return
+     * @throws FileNotFoundException 
+     */
     public SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> createGraph() throws FileNotFoundException{
+        //cria um objeto da classe FileReader
         FileReader f = new FileReader();
-        Set<Vertex> nós = f.reader();
+        //recebe o conjunto de nós com as informações preenchidas
+        Set<Vertex> nos = f.reader();
         SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> graph = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
-        for (Vertex nóAtual : nós) {
-            graph.addVertex(nóAtual);
+        //itera sobre o conjunto de nós para adiciona-los no grafo 
+        for (Vertex noAtual : nos) {
+            //adiciona o nó atual no grafo
+            graph.addVertex(noAtual);
+            //remove o nó atual do conjunto
+            nos.remove(noAtual);
+            //caso o conjunto de nós não esteja vazio
+            if(!nos.isEmpty()){
+                //percorre novamente o conjunto para adicionar as arestas
+                for (Vertex no : nos) {
+                    //caso o id do nó atual seja igual ao id de algum outro nó do conjunto
+                    if(noAtual.getId() == no.getId()){
+                        //adiciona uma aresta entre esses dois nós
+                        graph.addEdge(no, noAtual);
+                    }
+                }
+            }
         }
-        
+        //retorna o grafo preenchido com nós e arestas
         return graph;
     }
 }
