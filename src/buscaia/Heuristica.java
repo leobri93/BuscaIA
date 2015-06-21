3a28aa2389dc26e84c83aa5452fbbb0eff59e56d
vasconcellosv <viniciusdeck@gmail.com>
2015-06-21 15:29:13 -0300

    Classe Heurística
    Criação dos construtores
    Parametrização da heuristica (1 ou 2)
    Implementação da heuristica 1 (Distância Euclidiana)

--- src/buscaia/Heuristica.java
+++ src/buscaia/Heuristica.java
@@ -13,13 +13,69 @@ import org.jgrapht.graph.SimpleDirectedWeightedGraph;
  * @author Leonardo
  */
 public class Heuristica {
-    
-    
-    public int retornaPesoVertice(Vertex v){
-        return v.getPeso();
+
+    SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> graph;
+    int tipoHeuristica = 0;
+
+    Heuristica(SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> pGraph) {
+        this.graph = pGraph;
     }
     
-    public int retornaPesoAresta(SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> sg, DefaultWeightedEdge d){
+    Heuristica(SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> pGraph, int tipoHeuristica) {
+        this.graph = pGraph;
+        this.tipoHeuristica = tipoHeuristica;
+    }
+    
+    public void setTipoHeuristica(int tipoHeuristica){
+        this.tipoHeuristica = tipoHeuristica;
+    }
+
+    public double retornaPesoVertice(Vertex v) {
+        return v.getHeuristica();
+    }
+
+    public double retornaPesoAresta(SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> sg, DefaultWeightedEdge d) {
+        return sg.getEdgeWeight(d);
+    }
+    
+    /**
+     * Recebe um Vertex e retorna a heuristca de acordo com o tipo de heurística passado
+     * 
+     * @param atual
+     * @param end
+     * @return 
+     */
+    public double heuristica(Vertex atual, Vertex end){
+        if(tipoHeuristica == 1){
+           return heuristica1(atual, end);
+        }
+        if(tipoHeuristica == 2){
+            return heuristica2(atual, end);
+        }
         return 0;
+    }
+
+    /**
+     * Recebe um Vertex e retorna a heuristca (Distância Euclidiana)
+     *
+     * @param atual Vertex a ser calculada a heuristica
+     * @param end Vertex final (Objetivo a ser encontrado o caminho)
+     * @return
+     */
+    private double heuristica1(Vertex atual, Vertex end) {
+        double diferencaX = end.getLongitudeCentral() - atual.getLongitudeCentral();
+        double diferencaY = end.getLatitudeCentral() - atual.getLatitudeCentral();
+        return Math.pow(((diferencaX * diferencaX) + (diferencaY * diferencaY)), 1 / 2);
+    }
+
+    /**
+     * Recebe um Vertex e retorna a heuristca ()
+     * 
+     * @param atual Vertex a ser calculada a heuristica
+     * @param end Vertex final (Objetivo a ser encontrado o caminho)
+     * @return 
+     */
+    private double heuristica2(Vertex atual, Vertex end) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
