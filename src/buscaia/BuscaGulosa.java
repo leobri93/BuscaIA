2742d3c47c312f58f12e1dec84e537b4d077202d
Leonardo Brito <leobri93@gmail.com>
2015-06-24 18:01:15 -0300

    Revert "haha"
    This reverts commit d0a40ba9bcc16db8fd05ddb5ea5590da7c476f0e.

--- src/buscaia/BuscaGulosa.java
+++ src/buscaia/BuscaGulosa.java
@@ -7,7 +7,6 @@ package buscaia;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
@@ -15,69 +14,44 @@ import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 public class BuscaGulosa {
 
     //Graph definition
-    SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> graph = GraphCreation.createGraph();
+    SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> stringGraph = createStringGraph();
     List<Vertex> vertices = new LinkedList<>();
 
     public String BuscaGulosa(String start, String end) {
-        Queue<Vertex> frontier = new LinkedList<>();
+        List<Vertex> frontier = new LinkedList<>();
         List<Vertex> expanded = new LinkedList<>();
-        //Busca no grafo pela rua com nome star e coloca em initialVertex
+
         Vertex initialVertex = searchVertex(start);
-        //Busca no grafo pela rua com nome end e coloca em finalVertex
         Vertex finalVertex = searchVertex(end);
-        //Se eles não existirem
         if (initialVertex == null || finalVertex == null) {
-            //Se um dos vertices não existir
             return "Não existe um dos vertices";
         }
 
-        //Adiciona o vertice inicial ao frontier
         frontier.add(initialVertex);
 
-        Vertex removedFromFrontier = null;
+        Vertex removed = null;
         while (!expanded.contains(finalVertex)) {
 
-            //Se não tem ninguem no frontier
-            if (frontier.isEmpty()) {
-                //Erro
-                return "Não encontrado";
-            }
+            removed = frontier.remove(0);
 
-            //Remove o primeiro da fila do frontier
-            //removed = frontier.remove(0);
-            removedFromFrontier = frontier.poll();
-
-            //Se o no removido do frontier for a solução
-            if (removedFromFrontier.equals(end)) {
-                //Retorna a solução
-                return "Solução";
-            }
-            //Adiciona removedFromFrontier no expanded
-            expanded.add(removedFromFrontier);
-
-            //Pega todas as arestas com origem no removedFromFrontier
-            Set<DefaultWeightedEdge> temp = graph.edgesOf(removedFromFrontier);
-
-            //Percorre todas as arestas removidas
+            Set<DefaultWeightedEdge> temp = stringGraph.edgesOf(removed);
             for (DefaultWeightedEdge atual : temp) {
-                //Se o destino da aresta não esta no frontier nem no expanded
-                if ((!frontier.contains(graph.getEdgeTarget(atual))) && !expanded.contains(graph.getEdgeTarget(atual))) {
-                    //Passa o nome do target pro searchVertex para ele retornar o vertex com esse nome e add no frontier
-                    frontier.add(searchVertex(graph.getEdgeTarget(atual).getName()));
-                } else if ((frontier.contains(graph.getEdgeTarget(atual)) && (heuristica1(graph.getEdgeTarget(atual), finalVertex) < heuristica1(atual, finalVertex)))) {
-
+                if ((!frontier.contains(stringGraph.getEdgeTarget(atual))) && !expanded.contains(stringGraph.getEdgeTarget(atual))) {
+                    frontier.add(searchVertex(stringGraph.getEdgeTarget(atual).getName()));
+                }else if((!frontier.contains(stringGraph.getEdgeTarget(atual)))){
+                    
                 }
 
             }
-            expanded.add(removedFromFrontier);
+            expanded.add(removed);
 
             ordenaFrontier(frontier);
 
         }
         return "existe";
     }
-
 //Graph creation
+
     private static SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> createStringGraph() {
         SimpleDirectedWeightedGraph<Vertex, DefaultWeightedEdge> g = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
         Vertex Itaipu = new Vertex("Itaipu", 4);
@@ -114,16 +88,12 @@ public class BuscaGulosa {
     private void ordenaFrontier(List<Vertex> lista) {
         Vertex temp;
         for (int i = 0; i < lista.size(); i++) {
-            if (lista.get(i).getHeuristica() > lista.get(i++).getHeuristica()) {
+            if (lista.get(i).getPeso() > lista.get(i++).getPeso()) {
                 temp = lista.get(i++);
                 lista.set(i, lista.get(i++));
                 lista.set(i, temp);
             }
 
         }
-    }
-    
-    private double heuristica1(Vertex no, Vertex end){
-        return 0;
     }
 }
